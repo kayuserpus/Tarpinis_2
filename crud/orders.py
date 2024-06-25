944b0a4480f755c25a45259565d2c4fc9701d07e
PauleNajus <bundzapaulius@gmail.com>
2024-06-25 13:26:52 +0300

    Edited folder structure

--- crud/orders.py
+++
@@ -1,6 +0,0 @@
-from database import db
-from models import Order
-
-def get_all_orders():
-    data = db.session.query(Order).all()
-    return data
--- crud/user.py
+++
@@ -1,8 +0,0 @@
-# crud/user.py
-
-from database import db
-from models import User
-
-def get_all_users():
-    users = db.session.query(User).all()
-    return users
