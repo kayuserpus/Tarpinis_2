c69f1415f929080e63effa70459f390a682c52f2
PauleNajus <bundzapaulius@gmail.com>
2024-06-26 11:51:48 +0300

    Added 404 page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from config import Config
 from extensions import db, migrate, login_manager
 
@@ -13,7 +13,7 @@ def create_app():
 
     from routes.user import user_bp
     from routes.auth import auth_bp
-    from routes.admin import admin_bp 
+    from routes.admin import admin_bp
     
     app.register_blueprint(user_bp, url_prefix='/user')
     app.register_blueprint(auth_bp, url_prefix='/auth')
@@ -23,6 +23,14 @@ def create_app():
     def load_user(user_id):
         from models import User
         return User.query.get(int(user_id))
+
+    @app.route('/')
+    def index():
+        return render_template('shared/index.html')
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('shared/404.html'), 404
 
     with app.app_context():
         db.create_all()
