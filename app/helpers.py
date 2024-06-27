4647e83a7105c458f56a3759f9c7e8b0fb4998f7
W4LD3V <girstautas.valdemaras@gmail.com>
2024-06-27 15:10:53 +0300

    Added search funtionality, fixed header and cart pages

--- app/helpers.py
+++ app/helpers.py
@@ -1,13 +1,18 @@
-def get_products_and_categories(selected_category=''):
+def get_products_and_categories(selected_category='', search_query=''):
     from app.models import Product
     from app import db
     
     categories = db.session.query(Product.category).distinct().all()
-    categories = [c[0] for c in categories]  # Extract category names from tuples
+    categories = [c[0] for c in categories]
+    
+    query = Product.query
     
     if selected_category:
-        products = Product.query.filter_by(category=selected_category).all()
-    else:
-        products = Product.query.all()
+        query = query.filter_by(category=selected_category)
+    
+    if search_query:
+        query = query.filter(Product.name.ilike(f'%{search_query}%'))
+    
+    products = query.all()
     
     return products, categories
